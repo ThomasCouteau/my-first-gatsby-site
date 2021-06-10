0f7169a5c1d2b969b2656887d64094dea24041da
Thomas <mrthomas.couteau@gmail.com>
2021-06-10 14:40:42 +0200

    about

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Etape 1 : importation du composant
+import * as React from 'react'
+
+// Etape 2 : Def du composant
+const AboutPage = () => {
+  return (
+    <main>
+      <title>A propos de moi</title>
+      <h1>A propos de moi</h1>
+      <p>Salut à tous! Ceci est mon tout premier site que je construis avec Gatsby.</p>
+    </main>
+  )
+}
+
+// Step 3: Export your component
+export default AboutPage
