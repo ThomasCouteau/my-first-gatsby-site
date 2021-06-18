bf6d7347cddc866ab869fc2776c168a47357f8b7
Thomas <mrthomas.couteau@gmail.com>
2021-06-18 09:38:13 +0200

    photo ajoutée

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,15 +4,20 @@
 // Etape 1 : importation du composant
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 // Etape 2 : Def du composant
 const AboutPage = () => {
   return (
     <Layout pageTitle="A propos de moi">
-      <p>
-        Salut à tous! Ceci est mon tout premier site que je construis avec Gatsby. <br />
-        Je m'appelle Thomas, j'ai 19ans et je suis en BTS informatique au lycée Gustave Eiffel.
-      </p>
+        <p>
+          Salut à tous! Ceci est mon tout premier site que je construis avec Gatsby. <br />
+          Je m'appelle Thomas, j'ai 19ans et je suis en BTS informatique au lycée Gustave Eiffel.
+        </p>
+        <StaticImage
+          alt="Photo de moi lorsque j'étais en vacances."
+          src="../images/moi.JPG"
+       />
     </Layout>
   )
 }
